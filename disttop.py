26d9b8471b7f6bb9a52c8ce47d40ad3a471501d0
Thierry Delisle <tdelisle@uwaterloo.ca>
2020-08-18 12:49:58 -0400

    Refactored ssh-code into calltop function

--- disttop.py
+++ disttop.py
@@ -6,12 +6,31 @@ import subprocess
 
 target = "localhost"
 
-cmd = ["ssh", target, "-x", "top", "-bn", "1"]
-with subprocess.Popen(cmd, stdout=subprocess.PIPE) as proc:
-	try:
-		out, _ = proc.communicate()
+class SSH_Error(Exception):
+    """Exception raised for ssh errors .
 
-		print("Process returned code : {}".format(proc.returncode))
-		print( out.decode("utf-8") if out else "No output")
-	except subprocess.TimeoutExpired:
-		print("Command timeout")
+    Attributes:
+        error -- the stderr output of ssh
+        host  -- the host that triggered the error
+    """
+
+    def __init__(self, error, host):
+        self.error = error
+        self.host = host
+
+def calltop(host):
+	cmd = ["ssh", host, "-x", "top", "-bn", "1"]
+	with subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as proc:
+		out, err = proc.communicate()
+
+		if proc.returncode != 0:
+			raise SSH_Error(err.decode("utf-8") if err else "Unkown Error", host)
+
+		return out.decode("utf-8") if out else ""
+
+try:
+	out = calltop(target)
+	print(out)
+except SSH_Error as e:
+	print("Host {} encountered an error".format(e.host))
+	print(e.error)
